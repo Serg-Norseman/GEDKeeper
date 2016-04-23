59aa05cc59b40614aca3214f8b034bd61b89d493
Serg-Norseman <serg.zhdanovskih@yandex.ru>
2016-04-23 20:15:52 +0300

    .gitignore and first sample plugin

---
+++ projects/GKSamplePlugin/PluginForm.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Windows.Forms;
+
+using GKCore.Interfaces;
+
+namespace GKSamplePlugin
+{
+    public partial class PluginForm : Form
+    {
+        private readonly IPlugin plugin;
+
+        public PluginForm(IPlugin plugin)
+        {
+            InitializeComponent();
+            this.plugin = plugin;
+        }
+
+        private void frmP1Main_Load(object sender, EventArgs e)
+        {
+            tbInfo.AppendText(plugin.DisplayName + "\r\n");
+        }
+    }
+}
