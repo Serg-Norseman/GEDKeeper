b84c7b026d1cdcc211b8931399904def09552eb6
Serg-Norseman <serg.zhdanovskih@yandex.ru>
2016-04-23 22:09:19 +0300

    Calendar plugin

---
+++ projects/GKCalendarPlugin/GKCalendarPlugin.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+using GKCore.Interfaces;
+
+[assembly: AssemblyTitle("GKCalendarPlugin")]
+[assembly: AssemblyDescription("GEDKeeper2 Calendar plugin")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("GEDKeeper2")]
+[assembly: AssemblyCopyright("Copyright © 2014, Serg V. Zhdanovskih")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: CLSCompliant(false)]
+[assembly: ComVisible(false)]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+
+namespace GKCalendarPlugin
+{
+    public enum PLS
+    {
+		/* 033 */ LSID_MICalendar,
+		/* 140 */ LSID_Date,
+		/* 157 */ LSID_Cal_Gregorian,
+		/* 158 */ LSID_Cal_Julian,
+		/* 159 */ LSID_Cal_Hebrew,
+		/* 160 */ LSID_Cal_Islamic,
+		/* 161 */ LSID_Cal_Persian,
+		/* 162 */ LSID_Cal_Indian,
+		/* 163 */ LSID_Cal_Bahai,
+    }
+    
+    public sealed class Plugin : IPlugin, IWidget
+    {
+        private string fDisplayName = "GKCalendarPlugin";
+        private IHost fHost;
+        private ILangMan fLangMan;
+
+        public string DisplayName { get { return this.fDisplayName; } }
+        public IHost Host { get { return fHost; } }
+        public ILangMan LangMan { get { return fLangMan; } }
+
+        private TfmCalendar frm;
+        
+        public void Execute()
+        {
+			if (!this.fHost.IsWidgetActive(this)) {
+				frm = new TfmCalendar(this);
+				frm.Show();
+			} else {
+				frm.Close();
+			}
+        }
+
+        public void OnHostClosing(ref bool cancelClosing) {}
+		public void OnHostActivate() {}
+		public void OnHostDeactivate() {}
+
+		public void OnLanguageChange()
+        {
+        	try
+        	{
+        		this.fLangMan = this.fHost.CreateLangMan(this);
+        		this.fDisplayName = this.fLangMan.LS(PLS.LSID_MICalendar);
+        	}
+        	catch (Exception ex)
+        	{
+        		fHost.LogWrite("GKCalendarPlugin.OnLanguageChange(): " + ex.Message);
+        	}
+        }
+        
+        public bool Startup(IHost host)
+        {
+        	bool result = true;
+        	try
+        	{
+        		this.fHost = host;
+        		// Implement any startup code here
+        	}
+        	catch (Exception ex)
+        	{
+        		fHost.LogWrite("GKCalendarPlugin.Startup(): " + ex.Message);
+        		result = false;
+        	}
+        	return result;
+        }
+
+        public bool Shutdown()
+        {
+        	bool result = true;
+        	try
+        	{
+        		// Implement any shutdown code here
+        	}
+        	catch (Exception ex)
+        	{
+        		fHost.LogWrite("GKCalendarPlugin.Shutdown(): " + ex.Message);
+        		result = false;
+        	}
+        	return result;
+        }
+
+    	#region IWidget common
+
+    	void IWidget.WidgetInit(IHost host) {}
+        void IWidget.BaseChanged(IBaseWindow aBase) {}
+        void IWidget.BaseClosed(IBaseWindow aBase) {}
+        void IWidget.WidgetEnable() {}
+
+        #endregion
+    }
+}
---
+++ projects/GKCalendarPlugin/TfmCalendar.Designer.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace GKCalendarPlugin
+{
+	partial class TfmCalendar
+	{
+		private System.Windows.Forms.ListView lvDates;
+		private System.Windows.Forms.MonthCalendar qtc;
+		private System.Windows.Forms.ColumnHeader ColumnHeader1;
+		private System.Windows.Forms.ColumnHeader ColumnHeader2;
+
+		private void InitializeComponent()
+		{
+			this.lvDates = new System.Windows.Forms.ListView();
+			this.ColumnHeader1 = new System.Windows.Forms.ColumnHeader();
+			this.ColumnHeader2 = new System.Windows.Forms.ColumnHeader();
+			this.qtc = new System.Windows.Forms.MonthCalendar();
+			base.SuspendLayout();
+			this.lvDates.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] { this.ColumnHeader1, this.ColumnHeader2 });
+			this.lvDates.FullRowSelect = true;
+			this.lvDates.Location = new System.Drawing.Point(8, 184);
+			this.lvDates.Name = "lvDates";
+			this.lvDates.Size = new System.Drawing.Size(258, 121);
+			this.lvDates.TabIndex = 1;
+			this.lvDates.View = System.Windows.Forms.View.Details;
+			this.ColumnHeader1.Text = "Календарь";
+			this.ColumnHeader1.Width = 120;
+			this.ColumnHeader2.Text = "Дата";
+			this.ColumnHeader2.Width = 120;
+			this.qtc.FirstDayOfWeek = System.Windows.Forms.Day.Monday;
+			this.qtc.Location = new System.Drawing.Point(8, 8);
+			this.qtc.Name = "qtc";
+			this.qtc.ShowWeekNumbers = true;
+			this.qtc.TabIndex = 0;
+			this.qtc.DateSelected += new System.Windows.Forms.DateRangeEventHandler(this.qtc_DateSelected);
+			this.AutoScaleBaseSize = new System.Drawing.Size(5, 14);
+			base.ClientSize = new System.Drawing.Size(274, 313);
+			base.Controls.Add(this.qtc);
+			base.Controls.Add(this.lvDates);
+			this.Font = new System.Drawing.Font("Tahoma", 8.25f, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 204);
+			base.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+			this.Load += new System.EventHandler(this.TfmCalendar_Load);
+			base.Name = "TfmCalendar";
+			base.ShowInTaskbar = false;
+			base.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
+			this.Text = "Календарь";
+			base.TopMost = true;
+			base.Closed += new System.EventHandler(this.TfmCalendar_Closed);
+			base.ResumeLayout(false);
+		}
+	}
+}
