f51aa5c2d9e2c2829a68b70558ed9318b535417e
Serg-Norseman <serg.zhdanovskih@yandex.ru>
2016-04-23 21:54:56 +0300

    ImageViewer plugin

---
+++ projects/GKImageViewerPlugin/ImageViewer.Designer.cs
@@ -0,0 +1,71 @@
+﻿using System;
+
+namespace GKImageViewerPlugin
+{
+	partial class ImageViewer
+	{
+		private void InitializeComponent()
+		{
+			this.components = new System.ComponentModel.Container();
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ImageViewer));
+			this.ToolBar1 = new System.Windows.Forms.ToolBar();
+			this.tbFileLoad = new System.Windows.Forms.ToolBarButton();
+			this.ImageList_Buttons = new System.Windows.Forms.ImageList(this.components);
+			this.OpenDialog1 = new System.Windows.Forms.OpenFileDialog();
+			this.SuspendLayout();
+			// 
+			// ToolBar1
+			// 
+			this.ToolBar1.Appearance = System.Windows.Forms.ToolBarAppearance.Flat;
+			this.ToolBar1.Buttons.AddRange(new System.Windows.Forms.ToolBarButton[] {
+									this.tbFileLoad});
+			this.ToolBar1.ButtonSize = new System.Drawing.Size(27, 26);
+			this.ToolBar1.DropDownArrows = true;
+			this.ToolBar1.ImageList = this.ImageList_Buttons;
+			this.ToolBar1.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+			this.ToolBar1.Location = new System.Drawing.Point(0, 0);
+			this.ToolBar1.Name = "ToolBar1";
+			this.ToolBar1.ShowToolTips = true;
+			this.ToolBar1.Size = new System.Drawing.Size(792, 32);
+			this.ToolBar1.TabIndex = 1;
+			this.ToolBar1.ButtonClick += new System.Windows.Forms.ToolBarButtonClickEventHandler(this.ToolBar1_ButtonClick);
+			// 
+			// tbFileLoad
+			// 
+			this.tbFileLoad.ImageIndex = 0;
+			this.tbFileLoad.Name = "tbFileLoad";
+			this.tbFileLoad.Style = System.Windows.Forms.ToolBarButtonStyle.DropDownButton;
+			this.tbFileLoad.ToolTipText = "Открыть файл";
+			// 
+			// ImageList_Buttons
+			// 
+			this.ImageList_Buttons.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("ImageList_Buttons.ImageStream")));
+			this.ImageList_Buttons.TransparentColor = System.Drawing.Color.Transparent;
+			this.ImageList_Buttons.Images.SetKeyName(0, "");
+			// 
+			// OpenDialog1
+			// 
+			this.OpenDialog1.Filter = "Все файлы (*.*)|*.*";
+			// 
+			// ImageViewer
+			// 
+			this.AutoScaleBaseSize = new System.Drawing.Size(7, 17);
+			this.ClientSize = new System.Drawing.Size(792, 573);
+			this.Controls.Add(this.ToolBar1);
+			this.Font = new System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.KeyPreview = true;
+			this.Name = "ImageViewer";
+			this.ShowInTaskbar = false;
+			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+			this.Text = "Просмотр";
+			this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.TfmMediaView_KeyDown);
+			this.ResumeLayout(false);
+			this.PerformLayout();
+		}
+		private System.Windows.Forms.OpenFileDialog OpenDialog1;
+		private System.ComponentModel.IContainer components;
+		public System.Windows.Forms.ImageList ImageList_Buttons;
+		private System.Windows.Forms.ToolBarButton tbFileLoad;
+		private System.Windows.Forms.ToolBar ToolBar1;
+	}
+}
---
+++ projects/GKImageViewerPlugin/ImageViewer.cs
@@ -0,0 +1,150 @@
+﻿using System.Drawing;
+using System.IO;
+using System.Text;
+using System.Windows.Forms;
+
+using GKCommon.Controls;
+using GKCommon.GEDCOM;
+using GKCore.Interfaces;
+
+namespace GKImageViewerPlugin
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public partial class ImageViewer : Form, ILocalization
+	{
+		private ImageView fImageCtl;
+    	private readonly ILangMan fLangMan;
+
+    	public ImageViewer(IPlugin plugin)
+		{
+			this.InitializeComponent();
+
+            this.fLangMan = plugin.LangMan;
+
+            this.SetLang();
+		}
+
+		private void TfmMediaView_KeyDown(object sender, KeyEventArgs e)
+		{
+			if (e.KeyCode == Keys.Escape)
+			{
+				base.Close();
+			}
+		}
+
+		private void ToolBar1_ButtonClick(object sender, ToolBarButtonClickEventArgs e)
+		{
+			if (e.Button == this.tbFileLoad) {
+				if (OpenDialog1.ShowDialog() == DialogResult.OK) {
+					this.SetFileRef(OpenDialog1.FileName);
+				}
+			}
+		}
+
+		private void SetFileRef(string fileName)
+		{
+			this.SuspendLayout();
+
+			this.Text = fileName;
+			Control ctl = null;
+			this.fImageCtl = null;
+
+			GEDCOMMultimediaFormat fmt = GEDCOMFileReference.RecognizeFormat(fileName);
+			
+			switch (fmt)
+			{
+				case GEDCOMMultimediaFormat.mfBMP:
+				case GEDCOMMultimediaFormat.mfGIF:
+				case GEDCOMMultimediaFormat.mfJPG:
+				case GEDCOMMultimediaFormat.mfPCX:
+				case GEDCOMMultimediaFormat.mfTIF:
+				case GEDCOMMultimediaFormat.mfTGA:
+				case GEDCOMMultimediaFormat.mfPNG:
+					{
+						using (Stream fs = new FileStream(fileName, FileMode.Open))
+						{
+							this.fImageCtl = new ImageView();
+							this.fImageCtl.OpenImage(new Bitmap(fs));
+							ctl = this.fImageCtl;
+						}
+						break;
+					}
+
+				case GEDCOMMultimediaFormat.mfWAV:
+				case GEDCOMMultimediaFormat.mfAVI:
+				case GEDCOMMultimediaFormat.mfMPG:
+					break;
+
+				case GEDCOMMultimediaFormat.mfTXT:
+					{
+						using (Stream fs = new FileStream(fileName, FileMode.Open))
+						{
+							using (StreamReader strd = new StreamReader(fs, Encoding.GetEncoding(1251)))
+							{
+								TextBox txtBox = new TextBox();
+								txtBox.Multiline = true;
+								txtBox.ReadOnly = true;
+								txtBox.ScrollBars = ScrollBars.Both;
+								txtBox.Text = strd.ReadToEnd();
+								ctl = txtBox;
+							}
+						}
+						break;
+					}
+
+				case GEDCOMMultimediaFormat.mfRTF:
+					{
+						using (Stream fs = new FileStream(fileName, FileMode.Open))
+						{
+							using (StreamReader strd = new StreamReader(fs))
+							{
+								RichTextBox txtBox = new RichTextBox();
+								txtBox.ReadOnly = true;
+								txtBox.Text = strd.ReadToEnd();
+								ctl = txtBox;
+							}
+						}
+						break;
+					}
+
+				case GEDCOMMultimediaFormat.mfHTM:
+					{
+						using (Stream fs = new FileStream(fileName, FileMode.Open))
+						{
+							ctl = new WebBrowser();
+							(ctl as WebBrowser).DocumentStream = fs;
+						}
+						break;
+					}
+			}
+
+			if (ctl != null) {
+				ctl.Dock = DockStyle.Fill;
+				ctl.Location = new Point(0, 50);
+				ctl.Size = new Size(100, 100);
+				base.Controls.Add(ctl);
+				base.Controls.SetChildIndex(ctl, 0);
+			}
+
+			this.ResumeLayout(false);
+			this.PerformLayout();
+		}
+
+        #region ILocalization support
+
+        public void SetLang()
+        {
+        	this.Text = this.fLangMan.LS(IVLS.LSID_ImgViewer);
+        	
+            /*if (this.fImageCtl != null) {
+                this.fImageCtl.btnSizeToFit.Text = LangMan.LS(LSID.LSID_SizeToFit);
+                this.fImageCtl.btnZoomIn.Text = LangMan.LS(LSID.LSID_ZoomIn);
+                this.fImageCtl.btnZoomOut.Text = LangMan.LS(LSID.LSID_ZoomOut);
+            }*/
+        }
+
+        #endregion
+    }
+}
