135d3822ae0bc042cbe1d77d04717aea2c84bfbb
Ruslan Garipov <brigadir15@gmail.com>
2016-04-26 23:57:20 +0500

    Add makefile for 'GKCommon' project
    I've added makefile for 'GKCommon' project only.
    
    Because at this time makefile I've developed supports only Windows
    builds, in order to successfully build the 'GKCommon' project with this
    makefile you must use GNU make for MSYS2. You definitely will fail on
    Linux because Windows .NET toolchain requires me to use backslash, for
    example.
    
    See also: #5

---
+++ projects/values.mk
@@ -0,0 +1,12 @@
+﻿# Hardware platforms
+x86-64 := x86-64
+x86 := x86
+any := any
+# Software platforms
+windows := windows
+linux := linux
+# Release types
+debug := debug
+release := release
+# Projects
+gkcommon := GKCommon
---
+++ projects/windows.mk
@@ -0,0 +1,21 @@
+﻿# C# Compiler settings for Windows.
+
+windowscompilerdosany := C:\Program Files (x86)\MSBuild\14.0\bin\csc.exe
+windowscompilerdosx86-64 := $(windowscompilerdosany)
+windowscompilerdosx86 := $(windowscompilerdosany)
+windowsrcdosany := C:\Program Files (x86)\Microsoft SDKs\Windows\v10.0A\bin\NETFX 4.6 Tools\resgen.exe
+windowsrcdosx86-64 := $(windowsrcdosany)
+windowsrcdosx86 := $(windowsrcdosany)
+
+refsdos := C:\\Program Files (x86)\\Reference Assemblies\\Microsoft\\Framework\\.NETFramework\\v4.5.2\\
+
+coreresgenref := \
+//r:"$(refsdos)mscorlib.dll" \
+//r:"$(refsdos)System.Core.dll" \
+//r:"$(refsdos)System.Data.dll" \
+//r:"$(refsdos)System.dll" \
+//r:"$(refsdos)System.Drawing.dll" \
+//r:"$(refsdos)System.Management.dll" \
+//r:"$(refsdos)System.Runtime.Remoting.dll" \
+//r:"$(refsdos)System.Windows.Forms.dll" \
+//r:"$(refsdos)System.Xml.dll"
