5daed349c7e1e7f939e26907372c244c021a28b8
Serg-Norseman <serg.zhdanovskih@yandex.ru>
2016-05-12 21:06:12 +0500

    Improved generation of Debian package

--- deploy/gk_prepare_package.sh
+++ deploy/gk_prepare_package.sh
@@ -15,15 +15,17 @@ mkdir -p $BIN_PATH
 mkdir -p $LIB_PATH
 mkdir -p $PACK_PATH/usr/share/applications
 mkdir -p $PACK_PATH/usr/share/pixmaps
+mkdir -p $PACK_PATH/usr/share/mime/packages
 
 cp -r $DEV_PATH/deploy/DEBIAN/ ~/$PACK_PATH/
 
 cp $DEV_PATH/deploy/gk2_run.sh ~/$BIN_PATH
 chmod a+x ~/$BIN_PATH/gk2_run.sh
 
-cp $DEV_PATH/deploy/GEDKeeper2.png ~/$LIB_PATH
-cp $DEV_PATH/deploy/GEDKeeper2.png ~/$PACK_PATH/usr/share/pixmaps
-cp $DEV_PATH/deploy/GEDKeeper2.desktop ~/$PACK_PATH/usr/share/applications
+cp $DEV_PATH/deploy/gedkeeper.png ~/$LIB_PATH
+cp $DEV_PATH/deploy/gedkeeper.png ~/$PACK_PATH/usr/share/pixmaps
+cp $DEV_PATH/deploy/gedkeeper.desktop ~/$PACK_PATH/usr/share/applications
+cp $DEV_PATH/deploy/application-x-gedkeeper.xml ~/$PACK_PATH/usr/share/mime/packages
 
 cp $DEV_PATH/GEDKeeper2.exe ~/$LIB_PATH
 cp $DEV_PATH/GKCommon.dll ~/$LIB_PATH
@@ -35,10 +37,7 @@ cp $DEV_PATH/LuaInterface.dll ~/$LIB_PATH
 cp $DEV_PATH/ZedGraph.dll ~/$LIB_PATH
 cp $DEV_PATH/LICENSE ~/$LIB_PATH
 cp -r $DEV_PATH/locales/ ~/$LIB_PATH/
-
-#echo "..\plugins\" >> %lstfile%
-#echo "..\locales\" >> %lstfile%
-#echo "..\scripts\" >> %lstfile%
+cp -r $DEV_PATH/plugins/ ~/$LIB_PATH/
+cp -r $DEV_PATH/scripts/ ~/$LIB_PATH/
 
 dpkg -b ~/$PACK_PATH/
-#/home/norseman/gedkeeper-2.7.0
