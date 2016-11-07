ef74c4b42884c0cce71ab72670bd9b8a1fee53ca
Serg V. Zhdanovskih <serg.zhdanovskih@yandex.ru>
2016-11-07 17:20:21 +0500

    Added language's identifiers for GEDCOM

---
+++ projects/GKCommon/GEDCOM/GEDCOMLanguageID.cs
@@ -0,0 +1,116 @@
+/*
+ *  "GEDKeeper", the personal genealogical database editor.
+ *  Copyright (C) 2009-2016 by Serg V. Zhdanovskih (aka Alchemist, aka Norseman).
+ *
+ *  This file is part of "GEDKeeper".
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+using System;
+
+namespace GKCommon.GEDCOM
+{
+    public enum GEDCOMLanguageID
+    {
+        Unknown,
+        Afrikaans,
+        Albanian,
+        AngloSaxon,
+        Catalan,
+        CatalanSpn,
+        Czech,
+        Danish,
+        Dutch,
+        English,
+        Esperanto,
+        Estonian,
+        Faroese,
+        Finnish,
+        French,
+        German,
+        Hawaiian,
+        Hungarian,
+        Icelandic,
+        Indonesian,
+        Italian,
+        Latvian,
+        Lithuanian,
+        Navaho,
+        Norwegian,
+        Polish,
+        Portuguese,
+        Romanian,
+        SerboCroatian,
+        Slovak,
+        Slovene,
+        Spanish,
+        Swedish,
+        Turkish,
+        Wendic,
+
+        Amharic,
+        Arabic,
+        Armenian,
+        Assamese,
+        Belorusian,
+        Bengali,
+        Braj,
+        Bulgarian,
+        Burmese,
+        Cantonese,
+        ChurchSlavic,
+        Dogri,
+        Georgian,
+        Greek,
+        Gujarati,
+        Hebrew,
+        Hindi,
+        Japanese,
+        Kannada,
+        Khmer,
+        Konkani,
+        Korean,
+        Lahnda,
+        Lao,
+        Macedonian,
+        Maithili,
+        Malayalam,
+        Mandrin,
+        Manipuri,
+        Marathi,
+        Mewari,
+        Nepali,
+        Oriya,
+        Pahari,
+        Palio,
+        Panjabi,
+        Persian,
+        Prakrit,
+        Pusto,
+        Rajasthani,
+        Russian,
+        Sanskrit,
+        Serb,
+        Tagalog,
+        Tamil,
+        Telugu,
+        Thai,
+        Tibetan,
+        Ukrainian,
+        Urdu,
+        Vietnamese,
+        Yiddish
+    }
+}
