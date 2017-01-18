ee6ed1255b6ed8c252d3248f67daf3b026ff8199
g10101k <g101k@mail.ru>
2017-01-18 13:40:54 +0300

    Change copyright

--- projects/GEDKeeper2/GKUI/Controls/GKPortrait.cs
+++ projects/GEDKeeper2/GKUI/Controls/GKPortrait.cs
@@ -1,6 +1,6 @@
 ﻿/*
  *  "GEDKeeper", the personal genealogical database editor.
- *  Copyright (C) 2009-2016 by Serg V. Zhdanovskih (aka Alchemist, aka Norseman).
+ *  Copyright (C) 2016 by Serg V. Zhdanovskih, Igor Tyulyakov.
  *
  *  This file is part of "GEDKeeper".
  *
