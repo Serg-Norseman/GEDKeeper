110840142e6708f298adc5adca1704968d790359
Sergey V. Zhdanovskih <serg.zhdanovskih@yandex.ru>
2017-02-12 14:43:13 +0500

    Added support of MediaPlayer (based on nVLC wrapper), #84

---
+++ projects/GKMediaPlayer/MediaPlayer.cs
@@ -0,0 +1,200 @@
+﻿/*
+ *  "GEDKeeper", the personal genealogical database editor.
+ *  Copyright (C) 2017 by Sergey V. Zhdanovskih.
+ *
+ *  This file is part of "GEDKeeper".
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+using System;
+using System.ComponentModel;
+using System.Windows.Forms;
+
+using nVLC;
+using nVLC.Events;
+using nVLC.Media;
+using nVLC.Players;
+
+namespace GKMediaPlayer
+{
+    public partial class MediaPlayer : UserControl
+    {
+        #if !__MonoCS__
+        private const bool FIND_LIBVLC = true;
+        #else
+        private const bool FIND_LIBVLC = false;
+        #endif
+
+        private readonly IMediaPlayerFactory fFactory;
+        private readonly IDiskPlayer fPlayer;
+        private IMedia fMedia;
+
+        public MediaPlayer()
+        {
+            InitializeComponent();
+
+            fFactory = new MediaPlayerFactory(FIND_LIBVLC);
+            fPlayer = fFactory.CreatePlayer<IDiskPlayer>();
+
+            fPlayer.Events.PlayerPositionChanged += Events_PlayerPositionChanged;
+            fPlayer.Events.TimeChanged += Events_TimeChanged;
+            fPlayer.Events.MediaEnded += Events_MediaEnded;
+            fPlayer.Events.PlayerStopped += Events_PlayerStopped;
+
+            fPlayer.WindowHandle = pnlVideo.Handle;
+            trkVolume.Value = fPlayer.Volume > 0 ? fPlayer.Volume : 0;
+
+            UISync.Init(this);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void Events_PlayerStopped(object sender, EventArgs e)
+        {
+            UISync.Execute(InitControls);
+        }
+
+        private void Events_MediaEnded(object sender, EventArgs e)
+        {
+            UISync.Execute(InitControls);
+        }
+
+        private void InitControls()
+        {
+            trkPosition.Value = 0;
+            lblTime.Text = @"00:00:00";
+            lblDuration.Text = @"00:00:00";
+        }
+
+        private void Events_TimeChanged(object sender, MediaPlayerTimeChanged e)
+        {
+            UISync.Execute(() => lblTime.Text = TimeSpan.FromMilliseconds(e.NewTime).ToString().Substring(0, 8));
+        }
+
+        private void Events_PlayerPositionChanged(object sender, MediaPlayerPositionChanged e)
+        {
+            UISync.Execute(() => {
+                               var newPos = (int)(e.NewPosition * 100);
+                               if (newPos > trkPosition.Maximum) return;
+                               trkPosition.Value = newPos;
+                           });
+        }
+
+        private void LoadMedia()
+        {
+            using (var ofd = new OpenFileDialog())
+            {
+                if (ofd.ShowDialog() == DialogResult.OK)
+                {
+                    txtFileName.Text = ofd.FileName;
+                }
+            }
+        }
+
+        private void Events_StateChanged(object sender, MediaStateChange e)
+        {
+            UISync.Execute(() => label1.Text = e.NewState.ToString());
+        }
+
+        private void Events_DurationChanged(object sender, MediaDurationChange e)
+        {
+            UISync.Execute(() => lblDuration.Text = TimeSpan.FromMilliseconds(e.NewDuration).ToString().Substring(0, 8));
+        }
+
+        private void btnOpen_Click(object sender, EventArgs e)
+        {
+            LoadMedia();
+        }
+
+        private void btnPlay_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(txtFileName.Text))
+            {
+                fMedia = fFactory.CreateMedia<IMedia>(txtFileName.Text);
+                fMedia.Events.DurationChanged += Events_DurationChanged;
+                fMedia.Events.StateChanged += Events_StateChanged;
+                fMedia.Events.ParsedChanged += Events_ParsedChanged;
+
+                fPlayer.Open(fMedia);
+                fMedia.Parse(true);
+
+                fPlayer.Play();
+            }
+            else
+            {
+                errorProvider1.SetError(txtFileName, "Please select media path first !");
+            }
+        }
+
+        private void Events_ParsedChanged(object sender, MediaParseChange e)
+        {
+            Console.WriteLine(e.Parsed);
+        }
+
+        private void trkVolume_Scroll(object sender, EventArgs e)
+        {
+            fPlayer.Volume = trkVolume.Value;
+        }
+
+        private void trkPosition_Scroll(object sender, EventArgs e)
+        {
+            fPlayer.Position = (float)trkPosition.Value / 100;
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            fPlayer.Stop();
+        }
+
+        private void btnPause_Click(object sender, EventArgs e)
+        {
+            fPlayer.Pause();
+        }
+
+        private void btnMute_Click(object sender, EventArgs e)
+        {
+            fPlayer.ToggleMute();
+
+            btnMute.Text = fPlayer.Mute ? "Unmute" : "Mute";
+        }
+
+        private void txtFileName_TextChanged(object sender, EventArgs e)
+        {
+            errorProvider1.Clear();
+        }
+
+        private static class UISync
+        {
+            private static ISynchronizeInvoke fSync;
+
+            public static void Init(ISynchronizeInvoke sync)
+            {
+                fSync = sync;
+            }
+
+            public static void Execute(Action action)
+            {
+                fSync.BeginInvoke(action, null);
+            }
+        }
+    }
+}
--- projects/GKSandbox/MainForm.Designer.cs
+++ projects/GKSandbox/MainForm.Designer.cs
@@ -29,6 +29,7 @@
         private void InitializeComponent()
         {
             this.gkComboBox1 = new GKCommon.Controls.GKComboBox();
+            this.mediaPlayer1 = new GKMediaPlayer.MediaPlayer();
             this.SuspendLayout();
             // 
             // gkComboBox1
@@ -36,16 +37,26 @@
             this.gkComboBox1.DrawMode = System.Windows.Forms.DrawMode.OwnerDrawFixed;
             this.gkComboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.gkComboBox1.FormattingEnabled = true;
-            this.gkComboBox1.Location = new System.Drawing.Point(85, 93);
+            this.gkComboBox1.Location = new System.Drawing.Point(12, 12);
             this.gkComboBox1.Name = "gkComboBox1";
             this.gkComboBox1.Size = new System.Drawing.Size(289, 23);
             this.gkComboBox1.TabIndex = 0;
+            // 
+            // mediaPlayer1
+            // 
+            this.mediaPlayer1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.mediaPlayer1.Location = new System.Drawing.Point(0, 56);
+            this.mediaPlayer1.Margin = new System.Windows.Forms.Padding(4);
+            this.mediaPlayer1.Name = "mediaPlayer1";
+            this.mediaPlayer1.Size = new System.Drawing.Size(1022, 531);
+            this.mediaPlayer1.TabIndex = 1;
             // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(590, 334);
+            this.ClientSize = new System.Drawing.Size(1022, 587);
+            this.Controls.Add(this.mediaPlayer1);
             this.Controls.Add(this.gkComboBox1);
             this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "MainForm";
@@ -53,6 +64,7 @@
             this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.MainFormFormClosed);
             this.ResumeLayout(false);
         }
+        private GKMediaPlayer.MediaPlayer mediaPlayer1;
         private GKCommon.Controls.GKComboBox gkComboBox1;
     }
 }
