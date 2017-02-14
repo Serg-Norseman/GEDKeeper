858a1eabaecda6f1b8336e4d40261075c2c43bb7
Sergey V. Zhdanovskih <serg.zhdanovskih@yandex.ru>
2017-02-14 19:51:37 +0500

    Optimized lists and added sort arrows

--- projects/GKCommon/GKCommon/SysUtils.cs
+++ projects/GKCommon/GKCommon/SysUtils.cs
@@ -868,6 +868,48 @@ namespace GKCommon
             }
         }
 
+        public enum Direction {
+            Up,
+            Right,
+            Down,
+            Left
+        }
+
+        public static void DrawTriangle(Graphics gfx, Rectangle rect, Direction direction)
+        {
+            int halfWidth = rect.Width / 2;
+            int halfHeight = rect.Height / 2;
+            Point p0 = Point.Empty;
+            Point p1 = Point.Empty;
+            Point p2 = Point.Empty;
+
+            switch (direction)
+            {
+                case Direction.Up:
+                    p0 = new Point(rect.Left + halfWidth, rect.Top);
+                    p1 = new Point(rect.Left, rect.Bottom);
+                    p2 = new Point(rect.Right, rect.Bottom);
+                    break;
+                case Direction.Down:
+                    p0 = new Point(rect.Left + halfWidth, rect.Bottom);
+                    p1 = new Point(rect.Left, rect.Top);
+                    p2 = new Point(rect.Right, rect.Top);
+                    break;
+                case Direction.Left:
+                    p0 = new Point(rect.Left, rect.Top + halfHeight);
+                    p1 = new Point(rect.Right, rect.Top);
+                    p2 = new Point(rect.Right, rect.Bottom);
+                    break;
+                case Direction.Right:
+                    p0 = new Point(rect.Right, rect.Top + halfHeight);
+                    p1 = new Point(rect.Left, rect.Bottom);
+                    p2 = new Point(rect.Left, rect.Top);
+                    break;
+            }
+
+            gfx.FillPolygon(Brushes.Black, new Point[] { p0, p1, p2 });
+        }
+
         #endregion
     }
 }
