0e8e3b89ee01f671bfea30ffdc422f13fe1bbf70
Serg V. Zhdanovskih (Norseman) <serg.zhdanovskih@yandex.ru>
2017-05-25 21:30:05 +0300

    Minor improvement of projects, #131

--- projects/GKv3/GEDKeeper3.Gtk2/Properties/AssemblyInfo.cs
+++ projects/GKv3/GEDKeeper3.Gtk2/Properties/AssemblyInfo.cs
@@ -1,27 +1,14 @@
-﻿using System.Reflection;
-using System.Runtime.CompilerServices;
-
-// Information about this assembly is defined by the following attributes.
-// Change them to the values specific to your project.
+using System;
+using System.Reflection;
+using System.Resources;
 
 [assembly: AssemblyTitle("GEDKeeper3.Gtk2")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyProduct("GEDKeeper")]
+[assembly: AssemblyCopyright("Copyright © 2009-2017 by Sergey V. Zhdanovskih")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-
-// The assembly version has the format "{Major}.{Minor}.{Build}.{Revision}".
-// The form "{Major}.{Minor}.*" will automatically update the build and revision,
-// and "{Major}.{Minor}.{Build}.*" will update just the revision.
-
-[assembly: AssemblyVersion("1.0.*")]
-
-// The following attributes are used to specify the signing key for the assembly,
-// if desired. See the Mono documentation for more information about signing.
-
-//[assembly: AssemblyDelaySign(false)]
-//[assembly: AssemblyKeyFile("")]
-
+[assembly: AssemblyVersion("3.0.0.0")]
+[assembly: NeutralResourcesLanguage("en")]
