1f63d20b7b85a1a893cab77d0ce2f083f190ce4a
Sergey V. Zhdanovskih <serg.zhdanovskih@yandex.ru>
2017-06-08 09:55:26 +0500

    Update of projects' files, #131

--- projects/GKv3/GEDKeeper3.Gtk2/Program.cs
+++ projects/GKv3/GEDKeeper3.Gtk2/Program.cs
@@ -1,6 +1,19 @@
 ﻿using System;
+using System.Reflection;
+using System.Resources;
 using Eto;
 using Eto.Forms;
+
+[assembly: AssemblyTitle("GEDKeeper3.Gtk2")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("GEDKeeper")]
+[assembly: AssemblyCopyright("Copyright © 2009-2017 by Sergey V. Zhdanovskih")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: AssemblyVersion("3.0.0.0")]
+[assembly: NeutralResourcesLanguage("en")]
 
 namespace GEDKeeper3.Gtk2
 {
--- projects/GKv3/GEDKeeper3.WinForms/Program.cs
+++ projects/GKv3/GEDKeeper3.WinForms/Program.cs
@@ -1,6 +1,17 @@
 ﻿using System;
+using System.Reflection;
 using Eto;
 using Eto.Forms;
+
+[assembly: AssemblyTitle("GEDKeeper3.WinForms")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("GEDKeeper")]
+[assembly: AssemblyCopyright("Copyright © 2009-2017 by Sergey V. Zhdanovskih")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: AssemblyVersion("3.0.0.0")]
 
 namespace GEDKeeper3.WinForms
 {
