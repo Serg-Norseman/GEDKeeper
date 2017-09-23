77793bde5b3c490437a6a03628bfb92bb62f455d
Serg V. Zhdanovskih (Norseman) <serg.zhdanovskih@yandex.ru>
2017-09-23 14:20:04 +0300

    Reorganization of the old project

--- clean.cmd
+++ clean.cmd
@@ -13,26 +13,20 @@ del .\GKUpdater.log
 
 rem <<< Main assemblies cleaning >>>
 
-rmdir .\projects\GEDKeeper2\bin /s /q
-rmdir .\projects\GEDKeeper2\obj /s /q
+rmdir .\projects\GKv2\GEDKeeper2\bin /s /q
+rmdir .\projects\GKv2\GEDKeeper2\obj /s /q
 
 rmdir .\projects\GKCore\bin /s /q
 rmdir .\projects\GKCore\obj /s /q
 
-rmdir .\projects\GKCommon\bin /s /q
-rmdir .\projects\GKCommon\obj /s /q
-
-rmdir .\projects\GKComponents\bin /s /q
-rmdir .\projects\GKComponents\obj /s /q
-
-rmdir .\projects\GKMediaPlayer\bin /s /q
-rmdir .\projects\GKMediaPlayer\obj /s /q
+rmdir .\projects\GKv2\GKComponents\bin /s /q
+rmdir .\projects\GKv2\GKComponents\obj /s /q
 
 rmdir .\projects\GKIntl\bin /s /q
 rmdir .\projects\GKIntl\obj /s /q
 
-rmdir .\projects\GKSandbox\bin /s /q
-rmdir .\projects\GKSandbox\obj /s /q
+rmdir .\projects\GKv2\GKSandbox\bin /s /q
+rmdir .\projects\GKv2\GKSandbox\obj /s /q
 
 rmdir .\projects\GKTests\bin /s /q
 rmdir .\projects\GKTests\obj /s /q
@@ -44,44 +38,44 @@ rmdir .\projects\GKUpdater\obj /s /q
 
 rem <<< Plugins cleaning >>>
 
-rmdir .\projects\GKSamplePlugin\bin /s /q
-rmdir .\projects\GKSamplePlugin\obj /s /q
+rmdir .\projects\GKv2\GKSamplePlugin\bin /s /q
+rmdir .\projects\GKv2\GKSamplePlugin\obj /s /q
 
-rmdir .\projects\GKCalculatorPlugin\bin /s /q
-rmdir .\projects\GKCalculatorPlugin\obj /s /q
+rmdir .\projects\GKv2\GKCalculatorPlugin\bin /s /q
+rmdir .\projects\GKv2\GKCalculatorPlugin\obj /s /q
 
-rmdir .\projects\GKCalendarPlugin\bin /s /q
-rmdir .\projects\GKCalendarPlugin\obj /s /q
+rmdir .\projects\GKv2\GKCalendarPlugin\bin /s /q
+rmdir .\projects\GKv2\GKCalendarPlugin\obj /s /q
 
-rmdir .\projects\GKFlowInputPlugin\bin /s /q
-rmdir .\projects\GKFlowInputPlugin\obj /s /q
+rmdir .\projects\GKv2\GKFlowInputPlugin\bin /s /q
+rmdir .\projects\GKv2\GKFlowInputPlugin\obj /s /q
 
-rmdir .\projects\GKImageViewerPlugin\bin /s /q
-rmdir .\projects\GKImageViewerPlugin\obj /s /q
+rmdir .\projects\GKv2\GKImageViewerPlugin\bin /s /q
+rmdir .\projects\GKv2\GKImageViewerPlugin\obj /s /q
 
-rmdir .\projects\GKNamesBookPlugin\bin /s /q
-rmdir .\projects\GKNamesBookPlugin\obj /s /q
+rmdir .\projects\GKv2\GKNamesBookPlugin\bin /s /q
+rmdir .\projects\GKv2\GKNamesBookPlugin\obj /s /q
 
-rmdir .\projects\GKNavigatorPlugin\bin /s /q
-rmdir .\projects\GKNavigatorPlugin\obj /s /q
+rmdir .\projects\GKv2\GKNavigatorPlugin\bin /s /q
+rmdir .\projects\GKv2\GKNavigatorPlugin\obj /s /q
 
-rmdir .\projects\GKPedigreeImporterPlugin\bin /s /q
-rmdir .\projects\GKPedigreeImporterPlugin\obj /s /q
+rmdir .\projects\GKv2\GKPedigreeImporterPlugin\bin /s /q
+rmdir .\projects\GKv2\GKPedigreeImporterPlugin\obj /s /q
 
-rmdir .\projects\GKTextSearchPlugin\bin /s /q
-rmdir .\projects\GKTextSearchPlugin\obj /s /q
+rmdir .\projects\GKv2\GKTextSearchPlugin\bin /s /q
+rmdir .\projects\GKv2\GKTextSearchPlugin\obj /s /q
 
-rmdir .\projects\GKTimeLinePlugin\bin /s /q
-rmdir .\projects\GKTimeLinePlugin\obj /s /q
+rmdir .\projects\GKv2\GKTimeLinePlugin\bin /s /q
+rmdir .\projects\GKv2\GKTimeLinePlugin\obj /s /q
 
-rmdir .\projects\GKTreeVizPlugin\bin /s /q
-rmdir .\projects\GKTreeVizPlugin\obj /s /q
+rmdir .\projects\GKv2\GKTreeVizPlugin\bin /s /q
+rmdir .\projects\GKv2\GKTreeVizPlugin\obj /s /q
 
-rmdir .\projects\GKLifePlugin\bin /s /q
-rmdir .\projects\GKLifePlugin\obj /s /q
+rmdir .\projects\GKv2\GKLifePlugin\bin /s /q
+rmdir .\projects\GKv2\GKLifePlugin\obj /s /q
 
-rmdir .\projects\GKChroniclePlugin\bin /s /q
-rmdir .\projects\GKChroniclePlugin\obj /s /q
+rmdir .\projects\GKv2\GKChroniclePlugin\bin /s /q
+rmdir .\projects\GKv2\GKChroniclePlugin\obj /s /q
 
-rmdir .\projects\GKWordsCloudPlugin\bin /s /q
-rmdir .\projects\GKWordsCloudPlugin\obj /s /q
+rmdir .\projects\GKv2\GKWordsCloudPlugin\bin /s /q
+rmdir .\projects\GKv2\GKWordsCloudPlugin\obj /s /q
