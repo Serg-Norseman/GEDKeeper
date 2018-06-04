43cf836eabcb28bad934ba81eb1ff6db30b53c05
Serg V. Zhdanovskih (Norseman) <serg.zhdanovskih@yandex.ru>
2018-06-05 00:59:17 +0300

    Test fix for AppVeyor

--- build_all.mswin.x86.cmd
+++ build_all.mswin.x86.cmd
@@ -1,2 +1,3 @@
 C:\windows\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe projects\GEDKeeper2.mswin.sln /p:Configuration=Debug /p:Platform="x86"
 "C:\Program Files (x86)\NUnit 2.6.4\bin\nunit-console-x86.exe" projects\GKTests\bin\Debug\GKTests.dll
+pause
