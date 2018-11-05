72878410eb16d9ae23673ae94cd6edebf51a8500
Sergey V. Zhdanovskih <serg.zhdanovskih@yandex.ru>
2018-11-05 09:51:45 +0500

    Primary test for UWP

---
+++ projects/GKvU/GEDKeeperU/Properties/AssemblyInfo.cs
@@ -0,0 +1,13 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyTitle("GEDKeeperU")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyProduct("GEDKeeper")]
+[assembly: AssemblyCopyright("Copyright © 2009-2018 by Sergey V. Zhdanovskih")]
+[assembly: AssemblyVersion("4.0.0.0")]
+
+[assembly: AssemblyCulture("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyDelaySign(false)]
+[assembly: AssemblyKeyFile("")]
+[assembly: AssemblyKeyName("")]
---
+++ projects/GKvU/clean.cmd
@@ -0,0 +1,7 @@
+rem rmdir .\.vs /s /q
+
+rmdir .\GEDKeeperU\bin /s /q
+rmdir .\GEDKeeperU\obj /s /q
+
+rmdir .\GKComponents\bin /s /q
+rmdir .\GKComponents\obj /s /q
