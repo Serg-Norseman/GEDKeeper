e78e12b2b1a7db872c23674a6543eb05c3443308
Kevin Sandal <kdsandal@gmail.com>
2019-09-27 19:52:49 -0700

    Error checking, clean output

--- clean_all.cmd
+++ clean_all.cmd
@@ -1,18 +1,34 @@
+@ECHO OFF
 
-del .\GKCommon.*
-del .\GEDKeeper2.exe.*
-del .\GKUpdater.*
-del .\GKTray.*
-del .\*.dll
-del .\*.pdb
-del .\*.mdb
-del .\*.xml
-del .\*.log
-del .\plugins\*.* /q
-del .\deploy\*.zip /q
+@ECHO/
+@ECHO *****************
+@ECHO ** Cleaning up **
+@ECHO *****************
+@ECHO/
 
-del .\GEDKeeper3.Gtk2.exe.*
-del .\GEDKeeper3.Gtk3.exe.*
-del .\GEDKeeper3.Mac.exe.*
-del .\GEDKeeper3.WinForms.exe.*
-del .\GEDKeeper3.Wpf.exe.*
+if exist .\LICENSE GOTO Clean_Up
+@ECHO/
+@ECHO ERROR: You are not in the correct directory to run this.
+pause
+exit /b 1
+
+:Clean_Up
+    if exist .\GKCommon.*                   del /q .\GKCommon.*
+    if exist .\GEDKeeper2.exe.*             del /q .\GEDKeeper2.exe.*
+    if exist .\GKUpdater.*                  del /q .\GKUpdater.*
+    if exist .\GKTray.*                     del /q .\GKTray.*
+    if exist .\*.dll                        del /q .\*.dll
+    if exist .\*.pdb                        del /q .\*.pdb
+    if exist .\*.mdb                        del /q .\*.mdb
+    if exist .\*.xml                        del /q .\*.xml
+    if exist .\*.log                        del /q .\*.log
+    if exist .\plugins\*.*                  del /q .\plugins\*.*
+    if exist .\deploy\*.zip                 del /q .\deploy\*.zip
+
+    if exist .\GEDKeeper3.Gtk2.exe.*        del /q .\GEDKeeper3.Gtk2.exe.*
+    if exist .\GEDKeeper3.Gtk3.exe.*        del /q .\GEDKeeper3.Gtk3.exe.*
+    if exist .\GEDKeeper3.Mac.exe.*         del /q .\GEDKeeper3.Mac.exe.*
+    if exist .\GEDKeeper3.WinForms.exe.*    del /q .\GEDKeeper3.WinForms.exe.*
+    if exist .\GEDKeeper3.Wpf.exe.*         del /q .\GEDKeeper3.Wpf.exe.*
+
+    exit /b 0
