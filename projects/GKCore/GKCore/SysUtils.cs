fe4b01518f01b71a646dd224239f11eb32bbf715
Serg V. Zhdanovskih (Norseman) <serg.zhdanovskih@yandex.ru>
2021-04-14 00:10:30 +0300

    Fixed slow display of large notes with html markup inside

--- projects/GKCore/GKCore/SysUtils.cs
+++ projects/GKCore/GKCore/SysUtils.cs
@@ -217,5 +217,28 @@ namespace GKCore
         }
 
         #endregion
+
+        public static string StripHTML(string source)
+        {
+            char[] array = new char[source.Length];
+            int arrayIndex = 0;
+            bool inside = false;
+            for (int i = 0; i < source.Length; i++) {
+                char let = source[i];
+                if (let == '<') {
+                    inside = true;
+                    continue;
+                }
+                if (let == '>') {
+                    inside = false;
+                    continue;
+                }
+                if (!inside) {
+                    array[arrayIndex] = let;
+                    arrayIndex++;
+                }
+            }
+            return new string(array, 0, arrayIndex);
+        }
     }
 }
