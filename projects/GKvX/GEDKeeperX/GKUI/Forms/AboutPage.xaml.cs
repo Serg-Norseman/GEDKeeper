8a242d6a69ff1189dc5a91231281ed9e93be8775
Serg V. Zhdanovskih (Norseman) <serg.zhdanovskih@yandex.ru>
2021-05-16 22:11:51 +0300

    GKvU, GKvX: Restore and update (2)

---
+++ projects/GKvX/GEDKeeperX/GKUI/Forms/AboutPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace GKUI.Forms
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class AboutPage : ContentPage
+    {
+        public AboutPage()
+        {
+            InitializeComponent();
+            Title = "About";
+            LogoImage.Source = ImageSource.FromResource(@"Resources.gk_logo.png");
+            AppNameLabel.Text = GKCore.GKData.APP_TITLE + " " + GKCore.AppHost.GetAppVersion();
+        }
+
+        private void OpenWeb_Clicked(object sender, EventArgs e)
+        {
+            Device.OpenUri(new Uri("https://gedkeeper.github.io/"));
+        }
+    }
+}
---
+++ projects/GKvX/GEDKeeperX/GKUI/Platform/HomeMenuItem.cs
@@ -0,0 +1,16 @@
+﻿namespace GKUI.Platform
+{
+    public enum MenuItemType
+    {
+        Browse,
+        About,
+        UserRef
+    }
+
+    public class HomeMenuItem
+    {
+        public MenuItemType Id { get; set; }
+
+        public string Title { get; set; }
+    }
+}
