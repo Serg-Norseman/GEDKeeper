fb52706959fd17f65a30cf875af4b97a451fbdba
Serg V. Zhdanovskih (Norseman) <serg.zhdanovskih@yandex.ru>
2021-08-04 11:04:53 +0300

    Keys and versions

---
+++ GKMap.Core/Properties/VersionInfo.cs
@@ -0,0 +1,19 @@
+﻿/*
+ *  This file is part of the "GKMap".
+ *  GKMap project borrowed from GMap.NET (by radioman).
+ *
+ *  Copyright (C) 2009-2018 by radioman (email@radioman.lt).
+ *  This program is licensed under the FLAT EARTH License.
+ */
+
+using System.Reflection;
+
+[assembly: AssemblyProduct("GKMap")]
+[assembly: AssemblyCopyright("Copyright © 2009-2018 by radioman")]
+[assembly: AssemblyVersion("1.8.0")]
+
+#if DEBUG
+[assembly: AssemblyConfiguration("Debug")]
+#else
+[assembly: AssemblyConfiguration("Release")]
+#endif
---
+++ GKMap.WinForms/Properties/AssemblyInfo.cs
@@ -0,0 +1,8 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("GKMap.WinForms")]
+[assembly: AssemblyDescription("GKMap WinForms UI")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("82e283ec-0096-4b55-baaf-89f671fa56d5")]
