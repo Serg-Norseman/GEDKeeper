5e79e4a35240dfc8429089e13c4472f303fd05b2
Serg V. Zhdanovskih (Norseman) <serg.zhdanovskih@yandex.ru>
2021-08-05 21:34:45 +0300

    Yandex providers

---
+++ GKMap.Core/MapProviders/Yandex/YandexMapProvider.cs
@@ -0,0 +1,104 @@
+﻿/*
+ *  This file is part of the "GKMap".
+ *  GKMap project borrowed from GMap.NET (by radioman).
+ *
+ *  Copyright (C) 2009-2018 by radioman (email@radioman.lt).
+ *  This program is licensed under the FLAT EARTH License.
+ */
+
+using System;
+
+namespace GKMap.MapProviders.Yandex
+{
+    public abstract class YandexMapProviderBase : GMapProvider
+    {
+        private GMapProvider[] fOverlays;
+
+        protected string Version = "4.6.9";
+
+        public readonly string Server /*d{'_'}b*/ = /*{^_^}*/ Stuff.GString /*{"_"}*/ (/* ;}~ */"MECxW6okUK3Ir7a9ue/vIA=="/* ;}~ */);
+        public readonly string ServerRu /*d{'_'}b*/ = /*{^_^}*/ Stuff.GString /*{"_"}*/ (/* ;}~ */"MECxW6okUK0FRlRPbF0BQg=="/* ;}~ */);
+        public readonly string ServerCom /*d{'_'}b*/ = /*{^_^}*/ Stuff.GString/*{"_"}*/ (/* ;}~ */"MECxW6okUK2JNHOW5AuimA=="/* ;}~ */);
+
+        public override Guid Id
+        {
+            get {
+                throw new NotImplementedException();
+            }
+        }
+
+        public override string Name
+        {
+            get {
+                throw new NotImplementedException();
+            }
+        }
+
+        public override PureProjection Projection
+        {
+            get {
+                return MercatorProjectionYandex.Instance;
+            }
+        }
+
+        public override GMapProvider[] Overlays
+        {
+            get {
+                if (fOverlays == null) {
+                    fOverlays = new GMapProvider[] { this };
+                }
+                return fOverlays;
+            }
+        }
+
+        public override PureImage GetTileImage(GPoint pos, int zoom)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    /// <summary>
+    /// YandexMap provider
+    /// </summary>
+    public class YandexMapProvider : YandexMapProviderBase
+    {
+        public static readonly YandexMapProvider Instance = new YandexMapProvider();
+
+        private static readonly string UrlServer = "vec";
+        private static readonly string UrlFormat = "http://{0}0{1}.{7}/tiles?l=map&v={2}&x={3}&y={4}&z={5}&lang={6}";
+
+        private readonly Guid fId = new Guid("82DC969D-0491-40F3-8C21-4D90B67F47EB");
+        private readonly string fName = "YandexMap";
+
+        public override Guid Id
+        {
+            get {
+                return fId;
+            }
+        }
+
+        public override string Name
+        {
+            get {
+                return fName;
+            }
+        }
+
+        private YandexMapProvider()
+        {
+            RefererUrl = "http://" + ServerCom + "/";
+        }
+
+        public override PureImage GetTileImage(GPoint pos, int zoom)
+        {
+            string url = MakeTileImageUrl(pos, zoom, LanguageStr);
+
+            return GetTileImageUsingHttp(url);
+        }
+
+        private string MakeTileImageUrl(GPoint pos, int zoom, string language)
+        {
+            return string.Format(UrlFormat, UrlServer, GetServerNum(pos, 4) + 1, Version, pos.X, pos.Y, zoom, language, Server);
+        }
+    }
+}
---
+++ GKMap.Core/MapProviders/Yandex/YandexSatelliteMapProvider.cs
@@ -0,0 +1,58 @@
+﻿/*
+ *  This file is part of the "GKMap".
+ *  GKMap project borrowed from GMap.NET (by radioman).
+ *
+ *  Copyright (C) 2009-2018 by radioman (email@radioman.lt).
+ *  This program is licensed under the FLAT EARTH License.
+ */
+
+using System;
+
+namespace GKMap.MapProviders.Yandex
+{
+    /// <summary>
+    /// YandexSatelliteMap provider
+    /// </summary>
+    public class YandexSatelliteMapProvider : YandexMapProviderBase
+    {
+        public static readonly YandexSatelliteMapProvider Instance = new YandexSatelliteMapProvider();
+
+        private static readonly string UrlServer = "sat";
+        private static readonly string UrlFormat = "http://{0}0{1}.{7}/tiles?l=sat&v={2}&x={3}&y={4}&z={5}&lang={6}";
+
+        public new string Version = "3.135.0";
+
+        private readonly Guid fId = new Guid("2D4CE763-0F91-40B2-A511-13EF428237AD");
+        private readonly string fName = "YandexSatelliteMap";
+
+        public override Guid Id
+        {
+            get {
+                return fId;
+            }
+        }
+
+        public override string Name
+        {
+            get {
+                return fName;
+            }
+        }
+
+        private YandexSatelliteMapProvider()
+        {
+        }
+
+        public override PureImage GetTileImage(GPoint pos, int zoom)
+        {
+            string url = MakeTileImageUrl(pos, zoom, LanguageStr);
+
+            return GetTileImageUsingHttp(url);
+        }
+
+        private string MakeTileImageUrl(GPoint pos, int zoom, string language)
+        {
+            return string.Format(UrlFormat, UrlServer, GetServerNum(pos, 4) + 1, Version, pos.X, pos.Y, zoom, language, Server);
+        }
+    }
+}
