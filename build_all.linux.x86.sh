604e21051092806ff1177dce4aeed090b7bf90b6
Serg V. Zhdanovskih (Norseman) <serg.zhdanovskih@yandex.ru>
2021-10-26 22:12:34 +0300

    Added experimental SQLite database support for internal application tasks

--- build_all.linux.x86.sh
+++ build_all.linux.x86.sh
@@ -6,7 +6,7 @@
 #export LDFLAGS=-m64
 #export LD_LIBRARY_PATH=$PWD/external/lua/linux/lib64
 
-msbuild ./projects/GEDKeeper2.linux.sln /p:Configuration=Debug /p:Platform="x86" /p:MonoCS=true /p:TargetFrameworkVersion=v4.5
+xbuild ./projects/GEDKeeper2.linux.sln /p:Configuration=Debug /p:Platform="x86" /p:MonoCS=true /p:TargetFrameworkVersion=v4.5 /v:quiet
 
 export TEMP="$( mktemp -d )"
 nunit-console ./projects/GKTests/bin/Debug/GKTests.dll && rm -r "$TEMP"
