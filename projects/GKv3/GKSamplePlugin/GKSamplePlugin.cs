c063f909dd75721aebf687aa286afc5214c5a6b5
Serg V. Zhdanovskih (Norseman) <serg.zhdanovskih@yandex.ru>
2022-05-22 16:52:37 +0300

    GKv3: fixed plugin loading

--- projects/GKv3/GKSamplePlugin/GKSamplePlugin.cs
+++ projects/GKv3/GKSamplePlugin/GKSamplePlugin.cs
@@ -1,6 +1,6 @@
 ﻿/*
  *  "GEDKeeper", the personal genealogical database editor.
- *  Copyright (C) 2009-2020 by Sergey V. Zhdanovskih.
+ *  Copyright (C) 2009-2022 by Sergey V. Zhdanovskih.
  *
  *  This file is part of "GEDKeeper".
  *
@@ -27,7 +27,7 @@ using GKCore.Interfaces;
 [assembly: AssemblyTitle("GKSamplePlugin1")]
 [assembly: AssemblyDescription("GEDKeeper sample plugin")]
 [assembly: AssemblyProduct("GEDKeeper")]
-[assembly: AssemblyCopyright("Copyright © 2014 by Sergey V. Zhdanovskih")]
+[assembly: AssemblyCopyright("Copyright © 2014-2022 by Sergey V. Zhdanovskih")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyCulture("")]
 
