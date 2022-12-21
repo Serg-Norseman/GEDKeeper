49a1d2e43c7c73a658f40e4a0237f65d8826a8f0
Serg V. Zhdanovskih (Norseman) <serg.zhdanovskih@yandex.ru>
2022-12-21 20:29:49 +0300

    Framework has been updated to version 4.5.2

--- build_all.mswin.x86.cmd
+++ build_all.mswin.x86.cmd
@@ -4,7 +4,7 @@ set CONFIG_TYPE=Debug
 for %%a in (release Release RELEASE) do if (%%a)==(%1) SET CONFIG_TYPE=Release
 
 set MSBDIR=@%WINDIR%\Microsoft.NET\Framework\v4.0.30319
-%MSBDIR%\msbuild.exe projects\GEDKeeper2.mswin.sln /p:Configuration=%CONFIG_TYPE% /p:Platform="x86" /t:Rebuild /p:TargetFrameworkVersion=v4.5
+%MSBDIR%\msbuild.exe projects\GEDKeeper2.mswin.sln /p:Configuration=%CONFIG_TYPE% /p:Platform="x86" /t:Rebuild /p:TargetFrameworkVersion=v4.5.2
 
 set BUILD_STATUS=%ERRORLEVEL% 
 if %BUILD_STATUS%==0 goto test 
@@ -18,6 +18,6 @@ exit /b 1
 set NUNIT="nunit_not_found"
 @if exist "%PROGRAMFILES(X86)%\NUnit 2.6.4\bin\nunit-console-x86.exe" set NUNIT="%PROGRAMFILES(X86)%\NUnit 2.6.4\bin\nunit-console-x86.exe"
 @if exist "%PROGRAMFILES(X86)%\NUnit.org\nunit-console\nunit3-console.exe" set NUNIT=@"%PROGRAMFILES(X86)%\NUnit.org\nunit-console\nunit3-console.exe" --x86
-%NUNIT% projects\GKTests\bin\Debug\GKTests.dll --framework=v4.5
+%NUNIT% projects\GKTests\bin\Debug\GKTests.dll --framework=v4.5.2
 pause 
 exit /b 0
