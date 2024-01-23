4c858692280f40c337f36a7393db9b5341d61963
Sergey V. Zhdanovskih <serg.zhdanovskih@yandex.ru>
2024-01-23 20:58:00 +0300

    Minor fixes

--- projects/GKTests/GDModel/GDMLinesTests.cs
+++ projects/GKTests/GDModel/GDMLinesTests.cs
@@ -1,6 +1,6 @@
 ﻿/*
  *  "GEDKeeper", the personal genealogical database editor.
- *  Copyright (C) 2009-2023 by Sergey V. Zhdanovskih.
+ *  Copyright (C) 2009-2024 by Sergey V. Zhdanovskih.
  *
  *  This file is part of "GEDKeeper".
  *
