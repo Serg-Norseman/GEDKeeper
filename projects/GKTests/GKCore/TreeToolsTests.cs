2fd655c498386706f8fd89fa5116c9d2ad4fd00d
Alex Zaytsev <hazzik@gmail.com>
2025-06-23 16:44:51 +1000

    Run unit tests on pull request

--- projects/GKTests/GKCore/TreeToolsTests.cs
+++ projects/GKTests/GKCore/TreeToolsTests.cs
@@ -253,7 +253,7 @@ namespace GKCore
 
             // three records with errors + multimedia with a nonexistent file
             TreeInspector.CheckBase(fBaseWin, checksList, progress);
-            Assert.AreEqual(3, checksList.Count);
+            Assert.AreEqual(4, checksList.Count);
 
             Assert.AreEqual(TreeInspector.CheckDiag.cdStrangeSpouse, checksList[0].Diag);
             Assert.AreEqual(TreeInspector.CheckDiag.cdStrangeSpouse, checksList[1].Diag);
